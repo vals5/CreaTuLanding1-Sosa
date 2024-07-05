ef5dcaecd7f5b304d884f33bd6a4972064f5cea9
Valentina Sosa <valeens12@gmail.com>
2024-07-05 00:45:19 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".container .letter-b", {
+  duration: 1000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".container img", {
+  duration: 1000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".container .text-left", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".container .text-right", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".container .explore", {
+  duration: 1000,
+  delay: 2500,
+});
+
+ScrollReveal().reveal(".container .catalog", {
+  duration: 1000,
+  delay: 3500,
+});
+
+ScrollReveal().reveal(".container h5", {
+  duration: 1000,
+  interval: 500,
+  delay: 4000,
+});
+
+ScrollReveal().reveal(".footer p", {
+  duration: 1000,
+  delay: 4500,
+});
